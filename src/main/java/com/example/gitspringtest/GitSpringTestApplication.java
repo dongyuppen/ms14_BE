8a3e701b381a34ead351fce0636e380b3ea638f1
dongyuppen <dongyuppen@naver.com>
2026-04-03 17:44:24 +0900

    add spring Project

---
+++ src/main/java/com/example/gitspringtest/GitSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitspringtest/GitSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
